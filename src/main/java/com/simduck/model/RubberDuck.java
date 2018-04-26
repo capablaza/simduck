b9b0e78188f2f3e1db4200bff9857cb546534c01
carlos apablaza <c.apablaza.z@gmail.com>
2018-04-25 22:16:19 -0300

    override fly for rubber duck

--- src/main/java/com/simduck/model/RubberDuck.java
+++ src/main/java/com/simduck/model/RubberDuck.java
@@ -2,6 +2,11 @@ package com.simduck.model;
 
 public class RubberDuck extends Duck{
 
+    @Override
+    public void fly() {
+        System.out.println("I  can't fly");
+    }
+
     public void display() {
         System.out.println("I'm RubberDuck!");
     }
